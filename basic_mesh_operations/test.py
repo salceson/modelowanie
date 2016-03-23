75887d0aef9299536594553016f5dcb2b7fc096d
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-23 02:06:52 +0100

    Testss

--- basic_mesh_operations/test.py
+++ basic_mesh_operations/test.py
@@ -1,28 +1,75 @@
-from CGAL.CGAL_Kernel import Point_2
-from CGAL.CGAL_Triangulation_2 import Triangulation_2
-from CGAL.CGAL_Triangulation_2 import Triangulation_2_Vertex_circulator
-from CGAL.CGAL_Triangulation_2 import Triangulation_2_Vertex_handle
+# coding=utf-8
+from time import time
 
-points=[]
-points.append( Point_2(1,0) )
-points.append( Point_2(3,2) )
-points.append( Point_2(4,5) )
-points.append( Point_2(9,8) )
-points.append( Point_2(7,4) )
-points.append( Point_2(5,2) )
-points.append( Point_2(6,3) )
-points.append( Point_2(10,1) )
+from halfedge_operations import HalfedgeMeshOperations
+from vertices_faces_operations import VerticesFacesOperations
 
-t=Triangulation_2()
-t.insert(points)
+__author__ = "Michał Ciołczyk"
 
-vc = t.incident_vertices(t.infinite_vertex())
 
-if vc.hasNext():
-  done = vc.next()
-  iter=Triangulation_2_Vertex_handle()
-  while(1):
-    iter=vc.next()
-    print(iter.point())
-    if iter == done:
-      break
+def _test_operations(he_ops, ve_ops):
+    """
+    :type he_ops HalfedgeMeshOperations
+    :type ve_ops VerticesFacesOperations
+    """
+    # Find vertex neighbors
+    he_fvn_start = time()
+    he_ops.find_vertex_neighbors(50)
+    he_fvn_stop = time()
+    ve_fvn_start = time()
+    ve_ops.find_vertex_neighbors(50)
+    ve_fvn_stop = time()
+    print "\tFind vertex neighbors:"
+    print "\t\tHalfedge: ", he_fvn_stop - he_fvn_start, "seconds."
+    print "\t\tVertices/faces list: ", ve_fvn_stop - ve_fvn_start, "seconds."
+    # Find vertex's faces
+    he_fvf_start = time()
+    he_ops.find_vertex_faces(50)
+    he_fvf_stop = time()
+    ve_fvf_start = time()
+    ve_ops.find_vertex_faces(50)
+    ve_fvf_stop = time()
+    print "\tFind vertex faces:"
+    print "\t\tHalfedge: ", he_fvf_stop - he_fvf_start, "seconds."
+    print "\t\tVertices/faces list: ", ve_fvf_stop - ve_fvf_start, "seconds."
+    # Find face neighbors
+    he_ffn_start = time()
+    he_ops.find_face_neighbors(150)
+    he_ffn_stop = time()
+    ve_ffn_start = time()
+    ve_ops.find_face_neighbors(150)
+    ve_ffn_stop = time()
+    print "\tFind face neighbors:"
+    print "\t\tHalfedge: ", he_ffn_stop - he_ffn_start, "seconds."
+    print "\t\tVertices/faces list: ", ve_ffn_stop - ve_ffn_start, "seconds."
+    # Flip edges
+    he_fe_start = time()
+    he_ops.flip_faces(150, 151)
+    he_fe_stop = time()
+    ve_fe_start = time()
+    he_ops.flip_faces(150, 151)
+    ve_fe_stop = time()
+    print "\tFlip edge:"
+    print "\t\tHalfedge: ", he_fe_stop - he_fe_start, "seconds."
+    print "\t\tVertices/faces list: ", ve_fe_stop - ve_fe_start, "seconds."
+    # Has border
+    he_hb_start = time()
+    has_border = he_ops.has_border()
+    he_hb_stop = time()
+    ve_hb_start = time()
+    he_ops.has_border()
+    ve_hb_stop = time()
+    print "\tHas border: %s" % has_border
+    print "\t\tHalfedge: ", he_hb_stop - he_hb_start, "seconds."
+    print "\t\tVertices/faces list: ", ve_hb_stop - ve_hb_start, "seconds."
+
+
+if __name__ == "__main__":
+    tests = ['Armadillo', 'HappyBuddha', 'Bunny']
+    for test in tests:
+        print test
+        he_ops = HalfedgeMeshOperations('data/%s.off' % test)
+        ve_ops = VerticesFacesOperations('data/%s.off' % test)
+        _test_operations(he_ops, ve_ops)
+        del he_ops
+        del ve_ops
